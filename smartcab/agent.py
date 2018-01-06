b7fe43dec3e3e162d08beead4be0e81a0d79aede
Boy Kai <JToolesiii@gmail.com>
2018-01-06 16:39:25 -0500

    Feature: Add createQ() code for creating Q table for agent actions

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -90,8 +90,10 @@ class LearningAgent(Agent):
         # When learning, check if the 'state' is not in the Q-table
         # If it is not, create a new dictionary for that state
         #   Then, for each action available, set the initial Q-value to 0.0
-
-        return
+        if self.learning:
+            if state not in self.Q.keys():
+                for action in self.valid_actions:
+                    self.Q.update({state: {action: 0.0}})
 
 
     def choose_action(self, state):
@@ -130,7 +132,7 @@ class LearningAgent(Agent):
         ###########
         # When learning, implement the value iteration update rule
         #   Use only the learning rate 'alpha' (do not use the discount factor 'gamma')
-
+        
         return
 
 
